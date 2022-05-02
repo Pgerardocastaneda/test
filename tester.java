05319ec0c57072be2424476a723fa5469f90f197
Pgerardocastaneda <68565812+Pgerardocastaneda@users.noreply.github.com>
2022-05-01 17:53:44 -0700

    Added a test file

---
+++ tester.java
@@ -0,0 +1,13 @@
+import java.beans.Transient;
+
+
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class tester {
+    @Test
+    public void subtract(){
+        assertEquals(0, test.subtract(3, 2));
+        
+    }
+}
